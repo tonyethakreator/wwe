352dfeb3f904ca5e4f7735da93e83fccee2155fc
JEFFREY TAMUNOTONYE EMMY-WILLIE <tonye.official@gmail.com>
2025-10-09 15:34:57 +0100

    U go just dey stress person

--- remove_phone.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-# Find all HTML files in the pages directory
-for file in pages/*.html; do
-  # Replace the phone number line with an empty string
-  sed -i 's/<li><i class="fas fa-phone"><\/i> (123) 456-7890<\/li>//g' "$file"
-done
-
-echo "Phone numbers removed from all HTML files in pages directory."
